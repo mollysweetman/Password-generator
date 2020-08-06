cd599fe7cec036aeef77630c12b027ea920b50cf
mollysweetman <67077842+mollysweetman@users.noreply.github.com>
2020-08-06 17:18:17 -0600

    added loops, updated var, put everything in func

--- 02-Homework/Develop/assets/scripts/script.js
+++ 02-Homework/Develop/assets/scripts/script.js
@@ -1,7 +1,5 @@
 
   
-// Alert user to click the Generate Password button
-alert("Hello, Please click 'Generate Button' to start");
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -16,88 +14,82 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// function generatePassword() {}
+function generatePassword() {
 
+  //prompted for length of password between 8 and 128 characters
+  var lenPass = "";
+  console.log(lenPass);
 
+  while (isNaN(parseInt(lenPass)) || parseInt(lenPass) < 8 || parseInt(lenPass) > 128) {
+    lenPass = prompt("Please enter the length of password");
+  } 
 
-//prompted for length of password between 8 and 128 characters
-var lenPass = prompt("Please enter the length of password");
-// Converting user input from sting to integer
-parseInt(lenPass, 10); 
-parseInt(lenPass, 10) < 8
-parseInt(lenPass, 10) > 128
+  //Converting user input from string to integer
+  parseInt(lenPass, 10); 
+  parseInt(lenPass, 10) < 8;
+  parseInt(lenPass, 10) > 128;
 
-// Create variable that will store user input
-while (isNaN(parseInt(lenPass), 10) || parseInt((lenPass) ,10) < 8 || parseInt((lenPass) > 128)) {
-  prompt("Please enter the length of password");
+  // Create variable that will store user input
+
+  // Prompts after clicking "generate button"
+
+  var lowCase = confirm("Do you want lowercase letters?");
+
+  var uppCase = confirm("Do you want uppercase letters?");
+
+  var numbers = confirm("Do you want numbers?");
+
+  var specSym = confirm("Do you want special characters?");
+
+  // If user doesn't choose any options
+  while (!lowCase && !uppCase && !numbers && !specSym) {
+    lowCase = confirm("Do you want lowercase letters?")
+    uppCase = confirm("Do you want uppercase letters?");
+    numbers = confirm("Do you want numbers?");
+    specSym = confirm("Do you want special characters?");
+  }
+
+  //creating variable to collect user choices
+  var charSets = "";
+
+  var charLower = "abcdefghijklmnopqrstuvwxyz";
+  var charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var charNum = "0123456789";
+  var CharSym = "\\!#$%&()*+,-./:<=>?@[]^_`{|}~\'";
+
+  // adding lower case letters
+  if (lowCase) {
+    charSets += charLower;
+  }
+  console.log(charSets);
+
+  // adding upper case letters
+  if (uppCase) {
+    charSets += charUpper;
+  }
+  console.log(charSets);
+
+  // adding numbers
+  if (numbers) {
+    charSets += charNum
+  }
+  console.log(charSets);
+
+  // adding symbols
+  if  (specSym) {
+    charSets += CharSym;
+  }
+  console.log(charSets);
+  var randomPass = "";
+
+  // Create a for loop to generate a random password for user 
+  for (var i = 0; i < lenPass; i++) {
+  // randomly select password for user bases on choices given
+  var random = Math.floor(Math.random()  * charSets.length)
+
+  console.log(charSets[random]);
+  randomPass += charSets[random];
+  } 
+  return randomPass
+  console.log(randomPass);
 }
-
-// Prompts after clicking "generate button"
-
-var lowCase = confirm("Do you want lowercase letters?");
-
-var uppCase = confirm("Do you want uppercase letters?");
-
-var numbers = confirm("Do you want numbers?");
-
-var specChar = confirm("Do you want special characters?");
-
-
-// Set up variables for password characters
-var charLower = "abcdefghijklmnopqrstuvwxyz";
-var charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var charNum = "0123456789";
-var CharSym = "!#$%&()*+,-./:<=>?@^[]^_`{|}~";
-
-// creating variable to collect user choices
-var charSets = "";
-
-// adding lower case letters
-if (lowCase) {
-  charSets += charLower;
-}
-console.log(charSets);
-
-// adding upper case letters
-else (uppCase) {
-  charSets += charUpper;
-}
-console.log(charSets);
-
-// adding numbers
-else (numbers) {
-  charSets += charNum
-}
-console.log(charSets);
-
-// adding symbols
-else  (specChar) {
-  charSets += CharSym;
-}
-console.log(charSets);
-
-
-
-// think about what if user chooses none?
-
-// Create a for loop to generate a random password for user 
-for (var i = 0; i < lenPass; i++) {
-// randomly select password for user bases on choices given
-var random = Math.floor(Math.random()  * charSets.length)
-
-console.log(charSets[random]);
-password += charSets[random];
-}
-
-
-
-
-
-
-
-
-
-
-//prompted for character type (lowercase, uppercase, numeric, and/or special characters)
-// password is selected with matching criteria
-// password is displayed on screen for employee
